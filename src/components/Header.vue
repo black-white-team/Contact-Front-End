b2ad223a3880b5c409a637ca0834ee351d1525b3
W-jun-H <949920389@qq.com>
2024-11-23 11:48:19 +0800

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+    <div style="width: 500px; padding-left: 30px; font-weight: bold; color: dodgerblue">Basic Contact Functions</div>
+    <div style="flex: 1"></div>
+    <div style="width: 200px; padding-top: 10px; font-weight: bold">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          FZU: 832201321胡伟俊<i class="el-icon-arrow-down el-icon--right"></i>
+          <div style="flex: 1"></div>
+          MU: 22126538 Weijun_Hu<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
